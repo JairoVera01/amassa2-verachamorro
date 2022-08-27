7ed3184fe78a6e1fbe09981c9362b4d2c1aa0254
JairoVera01 <fj.verachamorro@gmail.com>
2022-08-27 17:41:13 -0500

    Se agrega Item Count

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemDetail = ({item}) => {
     const {id,producto,precio,cantidad,imgUrl,detalles} = item;
-
-return (
-    <div className="container">
-        <div className="row mt-5">
-            <div className="col">
-                <div className="row">
-                    <div className="col">
-                        <img src={imgUrl} alt="" className=" w-75 img-thumbnail rounded "/>
-                    </div>
-                    <div className="col text-start">
-                        <p>{producto}</p>
-                        <p>{detalles}</p>
-                        <p>S/.{precio}</p>
-                        <p>Stock {cantidad} u.</p>
+    const onAdd = (contador) =>{
+        alert("Se agregaron "+ contador)
+    }
+    return (
+        <div className="container">
+            <div className="row mt-5">
+                <div className="col">
+                    <div className="row">
+                        <div className="col">
+                            <img src={imgUrl} alt="" className=" w-75 img-thumbnail rounded "/>
+                            <ItemCount stock={cantidad} initial={1} onAdd={onAdd}/>
+                        </div>
+                        <div className="col text-start">
+                            <p>{producto}</p>
+                            <p>{detalles}</p>
+                            <p>S/.{precio}</p>
+                            <p>Stock {cantidad} u.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col">
+                <div className="col">
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default ItemDetail
