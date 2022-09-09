79b34bd5e46ca5dbbfb452c28771e026d100d371
JairoVera01 <fj.verachamorro@gmail.com>
2022-09-08 20:34:12 -0500

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,14 +12,16 @@ const Cart = () => {
         {items &&<div className="container">
             {items.map((item=>
             <div>
-                <div className="row row-cols-1 row-cols-md-3 ">
+                <div className="mt-4 row row-cols-1 row-cols-md-3 ">
                     <div className="col">
-                        <img className='w-75' src={item.imgUrl} alt="" />
+                        <img className='w-75 img-thumbnail' src={item.imgUrl} alt="" />
+                    </div>
+                    <div className="col text-start">
+                        <p className="h6">Producto {item.nombre} </p>
+                        <p className="h6">Cantidad seleccionada: {item.cantidad}</p>
                     </div>
                     <div className="col"></div>
-                    <div className="col"></div>
                 </div>
-                {item.nombre} - {item.cantidad}
             </div>
             ))}
         </div>}
