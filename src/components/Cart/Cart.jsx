ae0b3b31c2a93a1f90b7793ffb7c43b1cabd8121
JairoVera01 <fj.verachamorro@gmail.com>
2022-09-10 10:50:17 -0500

    Siguientes cambios

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,28 +1,48 @@
-import React , {useContext}from 'react'
-import { CartContext } from '../../context/CartContext'
+import React , {useContext} from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from '../../context/CartContext';
 
 
 const Cart = () => {
     const {items, removeItem, clear} =useContext(CartContext);
     return (
     <>
-        <div className="mt-5 pt-5">Carrito de compras</div>
-        {!items.length && <p className="mt-5 fw-bold">No hay items</p>}
-        {
+        <div className="container">
+            <p className="mt-5 pt-5 h3">Carrito de compras</p>
+            {!items.length ? 
+            <p className="mt-5 fw-bold">
+                No hay items
+                <Link to="/">
+                    <br />
+                    <button className="btn btn-dark ">Ir al inicio</button>
+                </Link>
+            </p>:
             
-            items.length && <ol>
-                {items.map(((item,indx)=> <li key={indx}>{item.nombre} - {item.quantity}
-                    <p>Precio por unidad {item.precio}</p>
-                    <br></br>
-                    <button className="btn btn-dark" onClick={()=>removeItem(item.id)}>Remover</button>
-                </li>      
-                ))}
-            </ol>
-        }
-        <hr />
-        Precio total de la compra = S/.{items.reduce((pv,cv)=> pv + (cv.precio*cv.quantity),0)}
-        <div>
-            <button onClick={clear}>Limpiar</button>
+                
+                items.length && <ol>
+                    {items.map(((item,indx)=> 
+                    <li key={indx}>
+                        <div className="row row-cols-1 row-cols-md-2">
+                            <div className="col">
+                                <img src={item.imgUrl} alt="" className="w-25 img-thumbnail" />
+                            </div>
+                            <div className="col text-start">
+                            {item.nombre} - {item.quantity}
+                                <p>Precio por unidad {item.precio}</p>
+                                <br></br>
+                                <button className="btn btn-dark" onClick={()=>removeItem(item.id)}>Remover</button>
+                            </div>
+                        </div>
+                        
+                    </li>      
+                    ))}
+                </ol>
+            }
+            <hr />
+            Precio total de la compra = S/.{items.reduce((pv,cv)=> pv + (cv.precio*cv.quantity),0)}
+            <div>
+                <button onClick={clear}>Limpiar</button>
+            </div>
         </div>
     </>
     )
